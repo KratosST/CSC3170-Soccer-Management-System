749bdf52f925bfc5f608f0a4e0afc18326907dd3
babi guling <oncwnuH1XLPCGOuGKIfk-CVJAfsQ@git.weixin.qq.com>
2020-04-17 23:40:57 +0800

    add按钮增加

---
+++ soccer/js/addNew.js
@@ -0,0 +1,7 @@
+document.getElementById('newForm').style.display = "none";
+function addNew() {
+    document.getElementById('newForm').style.display = "block";
+}
+function hidder() {
+    document.getElementById('newForm').style.display = "none";
+}
